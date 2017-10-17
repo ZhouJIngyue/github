00c6c2c0e0d66030ce91903a79969c52336079ca
ZhouJIngyue <32698786+ZhouJIngyue@users.noreply.github.com>
2017-10-17 23:09:03 +0800

    阶段性胜利.
    胜利个屁，就是查到头个字相同的..

---
+++ bahu/show/show/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace show
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_in = new System.Windows.Forms.TextBox();
+            this.button_search = new System.Windows.Forms.Button();
+            this.textBox_out = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_in
+            // 
+            this.textBox_in.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_in.Location = new System.Drawing.Point(12, 12);
+            this.textBox_in.Multiline = true;
+            this.textBox_in.Name = "textBox_in";
+            this.textBox_in.Size = new System.Drawing.Size(273, 180);
+            this.textBox_in.TabIndex = 1;
+            this.textBox_in.Text = "在此输入关键词（以;分隔）.";
+            // 
+            // button_search
+            // 
+            this.button_search.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_search.Location = new System.Drawing.Point(301, 9);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(268, 40);
+            this.button_search.TabIndex = 2;
+            this.button_search.Text = "搜索";
+            this.button_search.UseVisualStyleBackColor = true;
+            this.button_search.Click += new System.EventHandler(this.button_search_Click);
+            // 
+            // textBox_out
+            // 
+            this.textBox_out.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_out.Location = new System.Drawing.Point(301, 55);
+            this.textBox_out.Multiline = true;
+            this.textBox_out.Name = "textBox_out";
+            this.textBox_out.Size = new System.Drawing.Size(268, 137);
+            this.textBox_out.TabIndex = 3;
+            this.textBox_out.Text = "在此输出与输入关键词联系最紧密的关键词.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 205);
+            this.Controls.Add(this.textBox_out);
+            this.Controls.Add(this.button_search);
+            this.Controls.Add(this.textBox_in);
+            this.Name = "Form1";
+            this.Text = "Show";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_in;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.TextBox textBox_out;
+    }
+}
+
---
+++ bahu/show/show/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace show
+{
+    public partial class Form1 : Form
+    {
+        public string keyAddress = "E:\\data\\bahu\\key\\key.txt";
+        public string relationAddress = "E:\\data\\bahu\\relation\\relation.txt";
+        public List<string> keys = new List<string>();
+        public double[,] relation;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //读取key.
+            StreamReader rd = new StreamReader(keyAddress, Encoding.GetEncoding("gb2312"));
+            string s = rd.ReadLine();
+            while (s != null)
+            {
+                keys.Add(s);
+                s = rd.ReadLine();
+            }
+            //for (int i = 0; i < keys.Count; i++)
+            //{
+            //    textBox_out.Text += keys[i];
+            //}
+            relation = new double[keys.Count, keys.Count];
+            rd = new StreamReader(relationAddress);
+            for (int i = 0; i < keys.Count; i++)
+            {
+                string line = rd.ReadLine();
+                string[] data = line.Split(' ');
+                for (int j = 0; j < keys.Count; j++)
+                {
+                    relation[i, j] = double.Parse(data[j]);
+                    //textBox_out.Text = relation[i, j].ToString();
+                }
+            }
+        }
+
+        private void button_search_Click(object sender, EventArgs e)
+        {
+            //读入用户输入，并转化为索引.
+            List<int> keysIndex = new List<int>();
+            textBox_out.Text = "";
+            string textIn = textBox_in.Text;
+            string[] keysIn = textIn.Split(';');
+            for (int i = 0; i < keysIn.Length; i++)
+            {
+                for (int j = 0; j < keys.Count; j++)
+                {
+                    if (keys[j] == keysIn[i])
+                        keysIndex.Add(j);
+                }
+               // textBox_out.Text += relation[5, keysIndex[i]].ToString("E")+"\n";
+            }
+            //计算输入与所有的差异.
+            double[] difference = new double[keys.Count];
+            for (int i = 0; i < keys.Count; i++)
+            {
+                difference[i] = 0;
+            }
+            for (int i = 0; i < keysIndex.Count; i++)
+            {
+                for (int j = 0; j < keys.Count; j++)
+                {
+                    difference[j] += relation[keysIndex[i], j];
+                }
+            }
+            //排序.
+            double[] old_difference = new double[keys.Count];
+            for (int i = 0; i < keys.Count; i++)
+                old_difference[i]=difference[i];
+            for (int i = 0; i < keys.Count; i++)
+            {
+                for (int j = 0; j < keys.Count-1; j++)
+                {
+                    if (difference[j] > difference[j + 1])
+                    {
+                        double temp = difference[j];
+                        difference[j] = difference[j + 1];
+                        difference[j + 1] = temp;
+                    }
+                }
+            }
+            //输出最小差对应的名字.
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < keys.Count; j++)
+                {
+                    if(difference[i]==old_difference[j])
+                        textBox_out.Text += keys[j] + ";";
+                }
+            }
+        }
+    }
+}
